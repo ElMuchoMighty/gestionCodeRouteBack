c97713a4b6c93e1168621162e2ea676d376fef4d
ElMuchoMighty <guillaume.fambon@gmail.com>
2022-09-06 10:00:52 +0200

    initialisation du projet vide avec les dépendances

---
+++ src/main/java/com/inti/gestionCodeRouteBack/GestionCodeRouteBackApplication.java
@@ -0,0 +1,13 @@
+package com.inti.gestionCodeRouteBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCodeRouteBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCodeRouteBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inti/gestionCodeRouteBack/GestionCodeRouteBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inti.gestionCodeRouteBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCodeRouteBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
