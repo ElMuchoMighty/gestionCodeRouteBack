63979556606d20e91674ddfbd30237b6d5e2dc3d
GotaKev <kflesch.pro@gmail.com>
2022-09-14 22:48:07 +0200

    modif rendez vous et stats

--- src/main/java/com/inti/gestionCodeRouteBack/controller/RendezVousController.java
+++ src/main/java/com/inti/gestionCodeRouteBack/controller/RendezVousController.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.inti.gestionCodeRouteBack.entities.Cours;
 import com.inti.gestionCodeRouteBack.entities.RendezVous;
 import com.inti.gestionCodeRouteBack.service.impl.RendezVousService;
 
@@ -39,6 +41,14 @@ public class RendezVousController {
 	@DeleteMapping("/rendezVous/{idRendezVous}") 
 	public void deleteRendezVous(@PathVariable("idRendezVous") Long id) {
 		rendezVousService.delete(id);
+	}
+	
+	@PutMapping("/rendezVous/{idRendezVous}")
+	public RendezVous updateCoursWithPut(@PathVariable("idRendezVous") Long id, @RequestBody RendezVous rendezVous) { //
+		RendezVous currentUser = rendezVousService.findOne(id);
+		currentUser.setDateRendezVous(rendezVous.getDateRendezVous());
+		currentUser.setHeureRendezVous(rendezVous.getHeureRendezVous());
+		return rendezVousService.save(rendezVous);
 	}
 
 }
